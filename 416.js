be27883500a0ad4b4d0244481ca0c1a780369a2d
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-08-21 23:36:42 +0700

    416. Partition Equal Subset Sum

---
+++ 416.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums) {
+  const sum = nums.reduce((a, b) => a + b);
+  if (sum % 2 != 0) return false;
+  const target = sum / 2;
+  const dp = new Array(target + 1).fill(0);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = target; j >= nums[i]; j--) {
+      dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+    }
+  }
+  return dp[target] == target;
+};
