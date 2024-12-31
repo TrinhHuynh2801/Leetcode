59fc535de03c070cbcb3c7711e49fce021faef00
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-12-31 18:59:56 +0700

    322. Coin Change

--- 322.js
+++ 322.js
@@ -6,11 +6,9 @@
 var coinChange = function (coins, amount) {
   const dp = new Array(amount + 1).fill(amount + 1);
   dp[0] = 0;
-  for (let i = 1; i <= amount; i++) {
-    for (let coin of coins) {
-      if (i - coin >= 0) {
-        dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
-      }
+  for (let coin of coins) {
+    for (let cur = coin; cur <= amount; cur++) {
+      dp[cur] = Math.min(dp[cur], 1 + dp[cur - coin]);
     }
   }
   return dp[amount] !== amount + 1 ? dp[amount] : -1;
