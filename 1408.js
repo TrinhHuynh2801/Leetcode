f0a49fab7882764bbf67ff4c3f9fa2722c3566ea
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2025-01-07 21:38:52 +0700

    1408. String Matching in an Array

--- 1408.js
+++ 1408.js
@@ -7,8 +7,10 @@ var stringMatching = function (words) {
   words.sort((a, b) => a.length - b.length);
   for (let i = 0; i < words.length; i++) {
     for (let j = i + 1; j < words.length; j++) {
-      if (words[i].length > words[j].length) continue;
-      if (words[j].includes(words[i])) ans.push(words[i]);
+      if (words[j].includes(words[i])) {
+        ans.push(words[i]);
+        break;
+      }
     }
   }
   return ans;
