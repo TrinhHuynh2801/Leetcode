110803a53dee3e2432e864b451c0bd01fccc82e3
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2025-02-03 00:09:57 +0700

    1752. Check if Array Is Sorted and Rotated

--- 1752.js
+++ 1752.js
@@ -5,8 +5,11 @@
 var check = function (nums) {
   let rotate = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[(i + 1) % nums.length]) rotate++;
-    if (rotate > 1) return false;
+    if (nums[i] > nums[i + 1]) {
+      rotate++;
+    }
   }
-  return true;
+  if (rotate == 0) return true;
+  if (rotate == 1 && nums[0] >= nums[nums.length - 1]) return true;
+  return false;
 };
