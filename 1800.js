1ebe2a3e9cd596e3843bd5fd103ae70ecb135d2a
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2025-02-05 00:27:28 +0700

    1800. Maximum Ascending Subarray Sum

---
+++ 1800.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+  let max = nums[0];
+  let sum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      sum = nums[i];
+    } else sum += nums[i];
+    max = Math.max(sum, max);
+  }
+  return max;
+};
